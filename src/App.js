16458bc768c8ab21da9bb09dd82ede66d1324d10
oscar <oscar71156@gmail.com>
2022-12-27 15:54:59 +0800

    move layout context back to index.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import AppContent from "./components/AppContent";
-import { LayoutProvider } from "./contexts/Layout";
 function App() {
   return (
-    <LayoutProvider>
-      <div>
-        <AppContent />
-      </div>
-    </LayoutProvider>
+    <div>
+      <AppContent />
+    </div>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { LayoutProvider } from "./contexts/Layout";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <LayoutProvider>
+      <App />
+    </LayoutProvider>
   </React.StrictMode>
 );
 
