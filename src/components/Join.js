6e9f773e0ed6a2d65096300be36c45f74b003087
oscar <oscar71156@gmail.com>
2023-01-11 13:40:44 +0800

    change svg to png and add hover effect in Join Button

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,16 +1,16 @@
 import styled from "styled-components";
-import Icon from "../assets/icons";
-
 import iconJoinHand from "../assets/btn_joinHand.gif";
-
+import iconJoinBTN from "../assets/btn_join.png";
+import iconJoinBTNH from "../assets/btn_join_h.png";
 const JoinButtonCon = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
-const IconBTNJoin = styled(Icon.BTNJoin)`
+const ImageBTNJoin = styled.img`
   display: block;
   margin-top: 10px;
+  width: 103px;
 `;
 
 const ScheduleGIFImage = styled.img`
@@ -19,12 +19,16 @@ const ScheduleGIFImage = styled.img`
   visibility: ${(props) => (props.isShowHand ? "visible" : "hidden")};
 `;
 
-const JoinButton = ({ isShowHand = false,className }) => {
+const JoinButton = ({ isShowHand = false, className }) => {
   return (
     <JoinButtonCon className={className}>
       <ScheduleGIFImage src={iconJoinHand} isShowHand={isShowHand} />
       <a href="https://2022.thef2e.com/signup" target="_blank" rel="noreferrer">
-        <IconBTNJoin />
+        <ImageBTNJoin
+          src={iconJoinBTN}
+          onMouseEnter={(e) => (e.target.src = iconJoinBTNH)}
+          onMouseLeave={(e) => (e.target.src = iconJoinBTN)}
+        />
       </a>
     </JoinButtonCon>
   );
